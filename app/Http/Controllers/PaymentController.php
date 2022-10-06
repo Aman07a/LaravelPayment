b2f45aa51604ced9ac28616f4a94a0be046bea25
Aman Ahmed <aman16.aa17@gmail.com>
2022-10-06 12:51:49 +0200

    Updated PaymentController (pay,approval,cancelled)

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -4,17 +4,23 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Resolvers\PaymentPlatformResolver;
+use App\Services\PayPalService;
 
 class PaymentController extends Controller
 {
+    protected $paymentPlatformResolver;
+
     /**
      * Create a new controller instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(PaymentPlatformResolver $paymentPlatformResolver)
     {
         $this->middleware('auth');
+
+        $this->paymentPlatformResolver = $paymentPlatformResolver;
     }
 
     /**
@@ -24,7 +30,6 @@ class PaymentController extends Controller
      */
     public function pay(Request $request)
     {
-        // dump($request);
         $rules = [
             'value' => ['required', 'numeric', 'min:5'],
             'currency' => ['required', 'exists:currencies,iso'],
@@ -33,16 +38,32 @@ class PaymentController extends Controller
 
         $request->validate($rules);
 
-        return $request->all();
+        $paymentPlatform = $this->paymentPlatformResolver
+            ->resolveService($request->payment_platform);
+
+        session()->put('paymentPlatformId', $request->payment_platform);
+
+        return $paymentPlatform->handlePayment($request);
     }
 
     public function approval()
     {
-        //
+        if (session()->has('paymentPlatformId')) {
+            $paymentPlatform = $this->paymentPlatformResolver
+                ->resolveService(session()->get('paymentPlatformId'));
+
+            return $paymentPlatform->handleApproval();
+        }
+
+        return redirect()
+            ->route('home')
+            ->withErrors('We cannot retrieve your payment platform. Try again, please.');
     }
 
     public function cancelled()
     {
-        //
+        return redirect()
+            ->route('home')
+            ->withErrors('You cancelled the payment.');
     }
 }
