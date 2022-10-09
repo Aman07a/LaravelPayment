d5788cb4efc8ea3a073db9d667c784fcaa3203f5
Aman Ahmed <aman16.aa17@gmail.com>
2022-10-09 15:44:12 +0200

    Updated Subscription (Models, Migration)

--- app/Models/Subscription.php
+++ app/Models/Subscription.php
@@ -2,10 +2,29 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Plan;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Subscription extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'active_until',
+        'user_id',
+        'plan_id',
+    ];
+
+    protected $dates = [
+        'active_until',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function plan()
+    {
+        return $this->belongsTo(Plan::class);
+    }
 }
