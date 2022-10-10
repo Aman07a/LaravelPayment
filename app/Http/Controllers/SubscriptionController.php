44e152742502e796bc74d57f9a8411fd64352d51
Aman Ahmed <aman16.aa17@gmail.com>
2022-10-10 11:56:21 +0200

    Added new functions in SubscriptionController

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -6,5 +6,28 @@ use Illuminate\Http\Request;
 
 class SubscriptionController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function show()
+    {
+        dump('Processing...');
+    }
+
+    public function store()
+    {
+        // 
+    }
+
+    public function approval()
+    {
+        // 
+    }
+
+    public function cancelled()
+    {
+        // 
+    }
 }
