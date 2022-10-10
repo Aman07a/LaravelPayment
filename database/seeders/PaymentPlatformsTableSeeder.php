a77f6f5965085e3e4c8bc1c05967e7900a6c93cb
Aman Ahmed <aman16.aa17@gmail.com>
2022-10-10 15:41:22 +0200

    Added subscriptions_enabled to PaymentPlatformsTableSeeder

--- database/seeders/PaymentPlatformsTableSeeder.php
+++ database/seeders/PaymentPlatformsTableSeeder.php
@@ -17,11 +17,13 @@ class PaymentPlatformsTableSeeder extends Seeder
         PaymentPlatform::create([
             'name' => 'PayPal',
             'image' => 'img/payment-platforms/paypal.jpg',
+            'subscriptions_enabled' => true,
         ]);
 
         PaymentPlatform::create([
             'name' => 'Stripe',
             'image' => 'img/payment-platforms/stripe.jpg',
+            'subscriptions_enabled' => true,
         ]);
     }
 }
